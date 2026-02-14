20be82544b4a3a30be4696c0434eb8831bf4cda6
rubereco <96120261+rubereco@users.noreply.github.com>
2026-02-14 20:35:49 +0100

    changed pom

--- backend/src/test/java/com/aerogrid/backend/controller/StationIngestionControllerTest.java
+++ backend/src/test/java/com/aerogrid/backend/controller/StationIngestionControllerTest.java
@@ -9,7 +9,7 @@ import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.boot.test.mock.mockito.MockBean;
+import org.springframework.test.context.bean.override.mockito.MockitoBean;
 import org.springframework.http.MediaType;
 import org.springframework.test.web.servlet.MockMvc;
 
@@ -35,7 +35,7 @@ class StationIngestionControllerTest {
     @Autowired
     private ObjectMapper objectMapper;
 
-    @MockBean
+    @MockitoBean
     private CitizenIngestionService ingestionService;
 
     private StationIngestionDto validDto;
