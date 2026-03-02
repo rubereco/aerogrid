2b697e8591542bf23d921b39b64fab1cfe32b38e
rubereco <96120261+rubereco@users.noreply.github.com>
2026-03-02 20:52:42 +0100

    prep for security

---
+++ backend/src/main/java/com/aerogrid/backend/service/UserService.java
@@ -0,0 +1,14 @@
+package com.aerogrid.backend.service;
+
+import com.aerogrid.backend.repository.UserRepository;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+
+@Service
+@RequiredArgsConstructor
+public class UserService {
+
+    private final UserRepository userRepository;
+
+
+}
